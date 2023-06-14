7d8cb35d5ce236c77d449ac7ca26aa3461372f08
Min <sken6794@gmail.com>
2023-06-14 16:35:18 +0900

    last_last push

--- src/main/java/com/somo/util/filter/BoardFilter.java
+++ src/main/java/com/somo/util/filter/BoardFilter.java
@@ -1,41 +1,50 @@
 package com.somo.util.filter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter("/BoardFilter")
+@WebFilter({"/member/member_mypage.mem",
+	"/member/member_modify.mem",
+	"/member/member_myBoardList.mem"
+})
 public class BoardFilter implements Filter {
-       
+
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
 		request.setCharacterEncoding("utf-8");
-		
+
 		HttpServletRequest req = (HttpServletRequest)request;
 		HttpServletResponse res = (HttpServletResponse)response;
-		
+
 		HttpSession session = req.getSession();
-			
-		
+
+
 		String memId = (String)session.getAttribute("memId");
-		
-		String path = req.getContextPath() + "/member/member_login.mem";
-		
-		
-		
-	
-	
+
+		String path = req.getContextPath() + "/index.jsp";
+
+		if(memId == null) {
+
+			//res.sendRedirect(path);
+			res.setContentType("text/html; charset=UTF-8;");
+			PrintWriter out = res.getWriter();
+			out.println("<script>");
+			out.println("alert('허용하지 않는 접근입니다.');");
+			out.println("location.href='" + path + "';");
+			out.println("</script>");
+			return;
+		}
+
 		chain.doFilter(request, response);
 	}
-
-
 }
